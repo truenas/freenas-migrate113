00b25e34e118b6a27c8f845ac49fd1522894f507
themylogin <themylogin@gmail.com>
2020-04-29 18:43:07 +0200

    Also update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 rm ../migrate113_* || true
-tar -cf  ../migrate113_0.7.orig.tar * .git .idea
+tar -cf  ../migrate113_0.9.orig.tar * .git .idea
 gzip ../migrate113_*.orig.tar
 
 docker run --rm -v `pwd`/..:/work -it debian:stable sh -c 'cd /work/freenas-migrate113 && apt update && apt install -y build-essential debhelper-compat python3-all sudo && useradd user && sudo -u user dpkg-buildpackage -us -uc'
